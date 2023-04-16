aef16d0dd3da56fd5f30cf30ba467b3b38c505b4
amitmankikar <amitmankikar@gmail.com>
2023-04-16 18:02:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ import { check } from 'k6';
 
 
 export default function () {
-  const conn = udp.connect('localhost:33333');
-  //udp.writeLn(conn, 'Say Hello');
-  udp.writePkt(conn, 0x65);
-  let res = String.fromCharCode(...udp.read(conn, 1024))
+  const conn = udp.connect('localhost:14755');
+  console.log("send 0x000076afed839da8")
+  var seq = Math.floor(Math.random() * 1000) + 1;
+  var err = udp.writePktEx(conn, 3, 0, seq, 0x000076afed839da8);
+  let res = String.fromCharCode(...udp.readPkt(conn, seq, 1024))
   check (res, {
-    'verify ag tag': (res) => res.includes('d')
+    'verify ag tag': (res) => res.includes('Success')
   });
   udp.close(conn);
 }
