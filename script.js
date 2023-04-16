7ca69ef1f8714e44f5dc2c0d75c29aedf67b5c8d
amitmankikar <amitmankikar@gmail.com>
2023-04-16 22:20:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ import udp from 'k6/x/udp';
 import { check } from 'k6';
 
 
+
+/* our list of queries heres
+ */
+var queries = [
+0x000076afed,
+0x00012e9773,
+0x00018dc646,
+0x0001c1efe6,
+0x00020c61c8
+]
+
 export default function () {
+
   const conn = udp.connect('localhost:14755');
-  console.log("send 0x000076afed839da8")
   var seq = Math.floor(Math.random() * 1000) + 1;
-  var err = udp.writePktEx(conn, 3, 0, seq, 0x000076afed839da8);
+  var query = queries[Math.floor(Math.random()*queries.length)]
+  //console.log (query)
+  var err = udp.writePktEx(conn, 3, 0, seq, query);
   let res = String.fromCharCode(...udp.readPkt(conn, seq, 1024))
   check (res, {
     'verify ag tag': (res) => res.includes('Success')
